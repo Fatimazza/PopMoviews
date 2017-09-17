6c9186f25ea6f7bf9a4a25fb9abb1fbeb8c0b104
Fatima Azzahro <azzahro.fatima@gmail.com>
2017-09-17 22:46:03 +0700

    Add Item Menu for sorting list of movies

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/MoviesActivity.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/MoviesActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MoviesActivity extends AppCompatActivity {
 
@@ -26,5 +29,24 @@ public class MoviesActivity extends AppCompatActivity {
 
         mMoviesAdapter = new MoviesAdapter();
         mMoviesRecyclerView.setAdapter(mMoviesAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.movies, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sortby_popular:
+                Toast.makeText(this, "Menu " + getResources().getString(R.string.item_movie_popular), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_sortby_toprated:
+                Toast.makeText(this, "Menu " + getResources().getString(R.string.item_movie_top_rated), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
