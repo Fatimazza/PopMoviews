ba14d6b5490b446737fd0d7a8b42bfcb2f953f2d
Fatima Azzahro <azzahro.fatima@gmail.com>
2017-09-17 21:21:50 +0700

    Refactor activity and its layout name from Main to Movies Activity

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/MoviesActivity.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/MoviesActivity.java
@@ -3,11 +3,11 @@ package com.fatimazza.popmoviews.popmoviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movies);
     }
 }
