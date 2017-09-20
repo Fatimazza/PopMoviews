caef4eb60f807c9429a96b8f255d30df83e94fe2
Fatima Azzahro <azzahro.fatima@gmail.com>
2017-09-20 15:43:07 +0700

    Make MovieDetailDao implement Parcelable so that can be sent as Parcelable Intent

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/network/MovieDetailDao.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/network/MovieDetailDao.java
@@ -1,7 +1,10 @@
 package com.fatimazza.popmoviews.popmoviews.network;
 
 
-public class MovieDetailDao {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieDetailDao implements Parcelable {
 
     private int id;
     private double vote_average;
@@ -32,5 +35,41 @@ public class MovieDetailDao {
 
     public String getOverview() {
         return overview;
+    }
+
+    protected MovieDetailDao(Parcel in) {
+        id = in.readInt();
+        vote_average = in.readDouble();
+        title = in.readString();
+        poster_path = in.readString();
+        release_date = in.readString();
+        overview = in.readString();
+    }
+
+    public static final Creator<MovieDetailDao> CREATOR = new Creator<MovieDetailDao>() {
+        @Override
+        public MovieDetailDao createFromParcel(Parcel in) {
+            return new MovieDetailDao(in);
+        }
+
+        @Override
+        public MovieDetailDao[] newArray(int size) {
+            return new MovieDetailDao[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeDouble(vote_average);
+        parcel.writeString(title);
+        parcel.writeString(poster_path);
+        parcel.writeString(release_date);
+        parcel.writeString(overview);
     }
 }
