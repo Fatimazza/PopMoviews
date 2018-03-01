50feaa8fcff0609243e138d9d75df99b96ff3c1e
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-03-01 13:49:41 +0700

    Project 1B - Add DAO to get Movie Review data, Add service to call Movie Review API

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/network/TheMoviesServices.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/network/TheMoviesServices.java
@@ -2,6 +2,7 @@ package com.fatimazza.popmoviews.popmoviews.network;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 
@@ -14,5 +15,9 @@ public interface TheMoviesServices {
     @GET("movie/top_rated")
     Call<BaseListDao<MovieDetailDao>> fetchTopRatedMovies(
         @Query("api_key") String apiKey);
+
+    @GET("movie/{movie_id}/reviews")
+    Call<BaseListDao<MovieReviewDao>> getMovieReview(
+        @Path("movie_id") long movieId, @Query("api_key") String apiKey);
 
 }
