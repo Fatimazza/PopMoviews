57307d853b7db5ee8bffefc29d07025e4796e323
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-03-01 16:07:07 +0700

    Project 1B - Call API to load Movie Review & Movie Videos by its Id

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/activity/MovieDetailActivity.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/activity/MovieDetailActivity.java
@@ -1,15 +1,25 @@
 package com.fatimazza.popmoviews.popmoviews.activity;
 
+import com.fatimazza.popmoviews.popmoviews.BuildConfig;
 import com.fatimazza.popmoviews.popmoviews.R;
+import com.fatimazza.popmoviews.popmoviews.network.BaseListDao;
 import com.fatimazza.popmoviews.popmoviews.network.MovieDetailDao;
+import com.fatimazza.popmoviews.popmoviews.network.MovieReviewDao;
+import com.fatimazza.popmoviews.popmoviews.network.MovieVideoDao;
+import com.fatimazza.popmoviews.popmoviews.network.RetrofitHelper;
 import com.fatimazza.popmoviews.popmoviews.utils.Constant;
 import com.squareup.picasso.Picasso;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
@@ -66,6 +76,54 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
     private void callMovieDetailAPI(long movieId) {
+        //showLoading
+        loadReview(movieId);
+        loadVideos(movieId);
+    }
 
+    private void loadReview(long movieId) {
+        RetrofitHelper.getInstance().getMoviesServices()
+            .loadMovieReview(movieId, BuildConfig.API_KEY)
+            .enqueue(new Callback<BaseListDao<MovieReviewDao>>() {
+                @Override
+                public void onResponse(Call<BaseListDao<MovieReviewDao>> call,
+                    Response<BaseListDao<MovieReviewDao>> response) {
+                    if (response.body() != null) {
+                    } else {
+                        //show Error Messages
+                    }
+                    //hide loading Indicator
+                }
+
+                @Override
+                public void onFailure(Call<BaseListDao<MovieReviewDao>> call, Throwable t) {
+                    Log.d("retroFailure ", t.getMessage());
+                    //show Error Messages
+                    //hide loading Indicator
+                }
+            });
+    }
+
+    private void loadVideos(long movieId) {
+        RetrofitHelper.getInstance().getMoviesServices()
+            .loadMovieVideos(movieId, BuildConfig.API_KEY)
+            .enqueue(new Callback<BaseListDao<MovieVideoDao>>() {
+                @Override
+                public void onResponse(Call<BaseListDao<MovieVideoDao>> call,
+                    Response<BaseListDao<MovieVideoDao>> response) {
+                    if (response.body() != null) {
+                    } else {
+                        //show Error Messages
+                    }
+                    //hide loading Indicator
+                }
+
+                @Override
+                public void onFailure(Call<BaseListDao<MovieVideoDao>> call, Throwable t) {
+                    Log.d("retroFailure ", t.getMessage());
+                    //show Error Messages
+                    //hide loading Indicator
+                }
+            });
     }
 }
