196c2306766c5380e09d466b0870b29279c2e9f9
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-03-02 15:41:30 +0700

    Project 1B - Fav Movie Database - Change table and columns Modifier in Contract Class

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/data/FavoriteMoviesContract.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/data/FavoriteMoviesContract.java
@@ -7,14 +7,14 @@ public class FavoriteMoviesContract {
 
     public static final class FavoriteMoviesEntry implements BaseColumns {
 
-        private static final String TABLE_NAME = "favmovies";
+        public static final String TABLE_NAME = "favmovies";
 
-        private static final String COLUMN_MOVIE_ID = "movie_id";
-        private static final String COLUMN_MOVIE_TITLE = "movie_title";
-        private static final String COLUMN_MOVIE_VOTE = "movie_vote_average";
-        private static final String COLUMN_MOVIE_POSTER = "movie_poster_path";
-        private static final String COLUMN_MOVIE_RELEASE = "movie_release_date";
-        private static final String COLUMN_MOVIE_OVERVIEW = "movie_overview";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_MOVIE_TITLE = "movie_title";
+        public static final String COLUMN_MOVIE_VOTE = "movie_vote_average";
+        public static final String COLUMN_MOVIE_POSTER = "movie_poster_path";
+        public static final String COLUMN_MOVIE_RELEASE = "movie_release_date";
+        public static final String COLUMN_MOVIE_OVERVIEW = "movie_overview";
 
     }
 
