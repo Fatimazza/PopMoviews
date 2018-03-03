f736c5c4af3af8ba482e716476bbb75a6d3da9b3
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-03-03 21:18:56 +0700

    Project 1B - Fav Movie Content Provider - Add Content Provider constants to Contract class

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/data/FavoriteMoviesContract.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/data/FavoriteMoviesContract.java
@@ -1,11 +1,19 @@
 package com.fatimazza.popmoviews.popmoviews.data;
 
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class FavoriteMoviesContract {
 
+    public static final String AUTHORITY = "com.fatimazza.popmoviews.popmoviews";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" +AUTHORITY);
+    public static final String PATH_FAV_MOVIES = "favmovies";
+
     public static final class FavoriteMoviesEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+            BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAV_MOVIES).build();
 
         public static final String TABLE_NAME = "favmovies";
 
