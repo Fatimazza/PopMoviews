c3dd31d09c6cddc9ee2803b0818b8d10855536fe
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-03-05 13:19:17 +0700

    Project 1B - Fav Movie Database - Add Read data logic in Database Manager class

--- app/src/main/java/com/fatimazza/popmoviews/popmoviews/data/FavoriteMoviesDbManager.java
+++ app/src/main/java/com/fatimazza/popmoviews/popmoviews/data/FavoriteMoviesDbManager.java
@@ -5,8 +5,12 @@ import com.fatimazza.popmoviews.popmoviews.network.MovieDetailDao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavoriteMoviesDbManager {
 
@@ -38,6 +42,48 @@ public class FavoriteMoviesDbManager {
         if (insertedUri != null) {
             Log.d("Okhttp INSERT", insertedUri.toString());
         }
+    }
+
+    public static List<MovieDetailDao> readFavoriteMovie(Cursor cursor) {
+        List<MovieDetailDao> favoriteMovies = new ArrayList<>();
+
+        long id = -1;
+
+        try {
+
+            if (cursor.moveToFirst()) {
+                do {
+                    id = cursor.getLong(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry._ID));
+                    String movieId = cursor.getString(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry.COLUMN_MOVIE_ID));
+                    String movieTitle = cursor.getString(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry.COLUMN_MOVIE_TITLE));
+                    String movieVote = cursor.getString(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry.COLUMN_MOVIE_VOTE));
+                    String moviePosterPath = cursor.getString(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry.COLUMN_MOVIE_POSTER));
+                    String movieReleaseDate = cursor.getString(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry.COLUMN_MOVIE_RELEASE));
+                    String movieOverview = cursor.getString(cursor.getColumnIndexOrThrow(
+                        FavoriteMoviesContract.FavoriteMoviesEntry.COLUMN_MOVIE_OVERVIEW));
+
+                    MovieDetailDao movieDetail = new MovieDetailDao();
+                    movieDetail.setId(Long.parseLong(movieId));
+                    movieDetail.setTitle(movieTitle);
+                    movieDetail.setVote_average(Double.parseDouble(movieVote));
+                    movieDetail.setPoster_path(moviePosterPath);
+                    movieDetail.setRelease_date(movieReleaseDate);
+                    movieDetail.setOverview(movieOverview);
+                    favoriteMovies.add(movieDetail);
+
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        } catch (Exception e) {
+            Log.e("DBManager Error : ", e.toString());
+        }
+        return favoriteMovies;
     }
 
     public static void deleteFavoriteMovie(Context context, String id) {
